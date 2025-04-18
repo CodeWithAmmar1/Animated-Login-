132f4f351aa30045db78988f08582ee40a29d476
CodeWithAmmar1 <m.aliammar.naseer@gmail.com>
2025-04-18 14:13:33 +0500

    login page

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/material.dart' as material;
 import 'package:flutter/services.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:rive/rive.dart';
@@ -12,216 +13,218 @@ class LoginScreen extends StatefulWidget {
 }
 
 class _LoginScreenState extends State<LoginScreen> {
-  late String animationURL;
-  Artboard? _teddyArtboard;
-  SMITrigger? successTrigger, failTrigger;
-  SMIBool? isHandsUp, isChecking;
-  SMINumber? numLook;
-
-  bool _isChecked = false;
-
-  StateMachineController? stateMachineController;
-  final TextEditingController _emailController = TextEditingController();
-  final TextEditingController _passwordController = TextEditingController();
+  late String riveAnimationPath;
+  Artboard? teddyArtboard;
+  SMITrigger? triggerSuccess, triggerFail;
+  SMIBool? handsUpBool, checkingBool;
+  SMINumber? lookNumber;
+  StateMachineController? teddyController;
+  final TextEditingController emailCtrl = TextEditingController();
+  final TextEditingController passwordCtrl = TextEditingController();
+  bool isRememberMeChecked = false;
 
   @override
   void initState() {
     super.initState();
-    animationURL = defaultTargetPlatform == TargetPlatform.android ||
+    riveAnimationPath = defaultTargetPlatform == TargetPlatform.android ||
             defaultTargetPlatform == TargetPlatform.iOS
         ? 'assets/animations/login.riv'
         : 'animations/login.riv';
-    rootBundle.load(animationURL).then(
+    rootBundle.load(riveAnimationPath).then(
       (data) {
         final file = RiveFile.import(data);
         final artboard = file.mainArtboard;
-        stateMachineController =
+        teddyController =
             StateMachineController.fromArtboard(artboard, "Login Machine");
-        if (stateMachineController != null) {
-          artboard.addController(stateMachineController!);
+        if (teddyController != null) {
+          artboard.addController(teddyController!);
 
-          for (var e in stateMachineController!.inputs) {
-            debugPrint(e.runtimeType.toString());
-            debugPrint("name${e.name}End");
-          }
-
-          for (var element in stateMachineController!.inputs) {
-            if (element.name == "trigSuccess") {
-              successTrigger = element as SMITrigger;
-            } else if (element.name == "trigFail") {
-              failTrigger = element as SMITrigger;
-            } else if (element.name == "isHandsUp") {
-              isHandsUp = element as SMIBool;
-            } else if (element.name == "isChecking") {
-              isChecking = element as SMIBool;
-            } else if (element.name == "numLook") {
-              numLook = element as SMINumber;
+          for (var input in teddyController!.inputs) {
+            switch (input.name) {
+              case "trigSuccess":
+                triggerSuccess = input as SMITrigger;
+                break;
+              case "trigFail":
+                triggerFail = input as SMITrigger;
+                break;
+              case "isHandsUp":
+                handsUpBool = input as SMIBool;
+                break;
+              case "isChecking":
+                checkingBool = input as SMIBool;
+                break;
+              case "numLook":
+                lookNumber = input as SMINumber;
+                break;
             }
           }
         }
 
-        setState(() => _teddyArtboard = artboard);
+        setState(() => teddyArtboard = artboard);
       },
     );
   }
 
-  void handsOnTheEyes() {
-    isHandsUp?.change(true);
+  void onPasswordFieldTap() => handsUpBool?.change(true);
+
+  void onEmailFieldTap() {
+    handsUpBool?.change(false);
+    checkingBool?.change(true);
+    lookNumber?.change(0);
   }
 
-  void lookOnTheTextField() {
-    isHandsUp?.change(false);
-    isChecking?.change(true);
-    numLook?.change(0);
+  void onTextChanged(String value) {
+    lookNumber?.change(value.length.toDouble());
   }
 
-  void moveEyeBalls(val) {
-    numLook?.change(val.length.toDouble());
-  }
-
-  void login() {
-    isChecking?.change(false);
-    isHandsUp?.change(false);
-    if (_emailController.text == "ammar@gmail.com" &&
-        _passwordController.text == "admin") {
-      successTrigger?.fire();
+  void handleLogin() {
+    checkingBool?.change(false);
+    handsUpBool?.change(false);
+    if (emailCtrl.text == "ammar@gmail.com" &&
+        passwordCtrl.text == "ammar") {
+      triggerSuccess?.fire();
     } else {
-      failTrigger?.fire();
+      triggerFail?.fire();
     }
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: const Color(0xffd6e2ea),
-      body: Center(
-        child: SingleChildScrollView(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              if (_teddyArtboard != null)
-                SizedBox(
-                  width: 400,
-                  height: 300,
-                  child: Rive(
-                    artboard: _teddyArtboard!,
-                    fit: BoxFit.fitWidth,
+      body: Container(
+        width: double.infinity,
+        decoration:  BoxDecoration(
+          gradient: material.LinearGradient(
+    colors: [material.Color(0xffd6e2ea), material.Color(0xfff4f2f3)],
+    begin: material.Alignment.topLeft,
+    end: material.Alignment.bottomRight,
+  ),
+        ),
+        child: Center(
+          child: SingleChildScrollView(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                if (teddyArtboard != null)
+                  SizedBox(
+                    width: 400,
+                    height: 300,
+                    child: Rive(
+                      artboard: teddyArtboard!,
+                      fit: BoxFit.fitWidth,
+                    ),
                   ),
-                ),
-              Container(
-                alignment: Alignment.center,
-                width: 400,
-                padding: const EdgeInsets.only(bottom: 15),
-                margin: const EdgeInsets.only(bottom: 15 * 5),
-                decoration: BoxDecoration(
-                  color: Color.fromARGB(255, 224, 184, 195),
-                  borderRadius: BorderRadius.circular(10),
-                ),
-                child: Column(
-                  children: [
-                    Padding(
-                      padding: const EdgeInsets.symmetric(horizontal: 15),
-                      child: Column(
+                Container(
+                  width: 400,
+                  padding: const EdgeInsets.symmetric(
+                      horizontal: 20, vertical: 25),
+                  margin: const EdgeInsets.only(bottom: 40),
+                  decoration: BoxDecoration(
+                    color: const Color.fromARGB(255, 245, 220, 229),
+                    borderRadius: BorderRadius.circular(15),
+                    boxShadow: [
+                      BoxShadow(
+                        color: Colors.grey.shade300,
+                        offset: const Offset(0, 8),
+                        blurRadius: 15,
+                      )
+                    ],
+                  ),
+                  child: Column(
+                    children: [
+                      TextField(
+                        controller: emailCtrl,
+                        onTap: onEmailFieldTap,
+                        onChanged: onTextChanged,
+                        keyboardType: TextInputType.emailAddress,
+                        style: GoogleFonts.poppins(fontSize: 14),
+                        decoration: const InputDecoration(
+                          hintText: "Email",
+                          filled: true,
+                          border: OutlineInputBorder(
+                            borderRadius: BorderRadius.all(
+                              Radius.circular(12),
+                            ),
+                          ),
+                          focusedBorder: OutlineInputBorder(
+                            borderSide:
+                                BorderSide(color: Color(0xffb04863), width: 1.5),
+                          ),
+                        ),
+                      ),
+                      const SizedBox(height: 20),
+                      TextField(
+                        controller: passwordCtrl,
+                        onTap: onPasswordFieldTap,
+                        obscureText: true,
+                        style: GoogleFonts.poppins(fontSize: 14),
+                        decoration: const InputDecoration(
+                          hintText: "Password",
+                          filled: true,
+                          border: OutlineInputBorder(
+                            borderRadius: BorderRadius.all(
+                              Radius.circular(12),
+                            ),
+                          ),
+                          focusedBorder: OutlineInputBorder(
+                            borderSide:
+                                BorderSide(color: Color(0xffb04863), width: 1.5),
+                          ),
+                        ),
+                      ),
+                      const SizedBox(height: 20),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
                         children: [
-                          const SizedBox(height: 15 * 2),
-                          TextField(
-                            controller: _emailController,
-                            onTap: lookOnTheTextField,
-                            onChanged: moveEyeBalls,
-                            keyboardType: TextInputType.emailAddress,
-                            style: GoogleFonts.poppins(fontSize: 14),
-                            cursorColor: const Color(0xffb04863),
-                            decoration: const InputDecoration(
-                              hintText: "Email",
-                              filled: true,
-                              border: OutlineInputBorder(
-                                borderRadius: BorderRadius.all(
-                                  Radius.circular(10),
-                                ),
-                              ),
-                              focusColor: Color(0xffb04863),
-                              focusedBorder: OutlineInputBorder(
-                                borderSide: BorderSide(
-                                  color: Color(0xffb04863),
-                                ),
-                                borderRadius: BorderRadius.all(
-                                  Radius.circular(10),
-                                ),
-                              ),
-                            ),
-                          ),
-                          const SizedBox(height: 15),
-                          TextField(
-                            controller: _passwordController,
-                            onTap: handsOnTheEyes,
-                            keyboardType: TextInputType.visiblePassword,
-                            obscureText: true,
-                            style: GoogleFonts.poppins(fontSize: 14),
-                            cursorColor: const Color(0xffb04863),
-                            decoration: const InputDecoration(
-                              hintText: "Password",
-                              filled: true,
-                              border: OutlineInputBorder(
-                                borderRadius: BorderRadius.all(
-                                  Radius.circular(10),
-                                ),
-                              ),
-                              focusColor: Color(0xffb04863),
-                              focusedBorder: OutlineInputBorder(
-                                borderSide: BorderSide(
-                                  color: Color(0xffb04863),
-                                ),
-                                borderRadius: BorderRadius.all(
-                                  Radius.circular(10),
-                                ),
-                              ),
-                            ),
-                          ),
-                          const SizedBox(height: 15),
                           Row(
-                            crossAxisAlignment: CrossAxisAlignment.end,
-                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
                             children: [
-                              Row(
-                                children: [
-                                  Checkbox(
-                                    activeColor: const Color(0xffb04863),
-                                    value: _isChecked,
-                                    onChanged: (value) {
-                                      setState(() {
-                                        _isChecked = value!;
-                                      });
-                                    },
-                                  ),
-                                  Text(
-                                    "Remember me",
-                                    style: GoogleFonts.poppins(),
-                                  ),
-                                ],
+                              Checkbox(
+                                activeColor: const Color(0xffb04863),
+                                value: isRememberMeChecked,
+                                onChanged: (value) {
+                                  setState(() {
+                                    isRememberMeChecked = value!;
+                                  });
+                                },
                               ),
-                              ElevatedButton(
-                                onPressed: login,
-                                style: ElevatedButton.styleFrom(
-                                  backgroundColor: const Color(0xffb04863),
-                                ),
-                                child: Text(
-                                  "Login",
-                                  style: GoogleFonts.poppins(
-                                    color: Colors.white,
-                                    fontWeight: FontWeight.w600,
-                                  ),
-                                ),
+                              Text(
+                                "Remember me",
+                                style: GoogleFonts.poppins(fontSize: 13),
                               ),
                             ],
                           ),
+                          ElevatedButton(
+                            onPressed: handleLogin,
+                            style: ElevatedButton.styleFrom(
+                              backgroundColor: const Color(0xffb04863),
+                              padding: const EdgeInsets.symmetric(
+                                  horizontal: 24, vertical: 12),
+                              shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(10),
+                              ),
+                            ),
+                            child: Text(
+                              "Login",
+                              style: GoogleFonts.poppins(
+                                color: Colors.white,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                          ),
                         ],
                       ),
-                    ),
-                  ],
+                    ],
+                  ),
                 ),
-              ),
-              Text("© All Rights Reserved")
-            ],
+                Text(
+                  "© All Rights Reserved",
+                  style: GoogleFonts.poppins(
+                    fontSize: 12,
+                    color: Colors.grey.shade700,
+                  ),
+                )
+              ],
+            ),
           ),
         ),
       ),
